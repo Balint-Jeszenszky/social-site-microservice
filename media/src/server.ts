00d46e99311b7c1d2b556194a807d06a3eab2934
Balint Jeszenszky <kisjeszeni1234@gmail.com>
2021-10-31 02:46:09 +0200

    media service compose fix

--- media/src/server.ts
+++ media/src/server.ts
@@ -1,12 +1,12 @@
-import express, { ErrorRequestHandler, Request, Response } from 'express';
+import express, { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import router from './routes/routes';
 
 const app = express();
 
 app.use('/api/media', router);
 
-app.use((err: ErrorRequestHandler, req: Request, res: Response) => {
-    res.status(500).end('Something went wrong...');
+app.use((err: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).send('Something went wrong...');
     console.log(`Error: ${err}`);
 });
 
